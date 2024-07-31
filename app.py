aceb28832f2e84df6d06fa3a2104a171a43a9a7a
SimonRasel <112686121+SimonRasel@users.noreply.github.com>
2024-07-31 16:52:56 +0200

    Update app.py to simple hello world

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-import os
 from flask import Flask
-from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return "Welcome!"
-
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
-
-@app.route('/current-time')
-def current_time():
-    now = datetime.now()
-    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f"The current time is {current_time}"
+def hello_world():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
